50fe947637285e34b1371f11558bf4a09c8b0523
Elric69 <97374362+Elric69@users.noreply.github.com>
2024-10-27 16:29:58 +0530

    Add files via upload

---
+++ msrc/app.py
@@ -0,0 +1,26 @@
+import requests
+from flask import (
+    Flask,
+    render_template,
+    jsonify,
+    request
+)
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("chat.html")
+
+@app.route("/getData", methods =["GET","POST"])
+def fetch():
+    data = request.json
+    message = data.get("message")
+    url = f"http://api.brainshop.ai/get?bid=180356&key=6DRvcrqFlApaokis&uid=1&msg={message}"
+    res = requests.get(url)
+    response = res.json()
+    return jsonify(response)
+
+
+if __name__ == "__main__":
+    app.run(port = 1234, debug = True)
