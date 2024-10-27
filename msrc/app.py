2cc0d657465ab531a39be8626bbd801ea414ef84
Elric69 <97374362+Elric69@users.noreply.github.com>
2024-10-27 22:01:46 +0530

    Update app.py

--- msrc/app.py
+++ msrc/app.py
@@ -1,9 +1,6 @@
-import requests
 from flask import (
     Flask,
-    render_template,
-    jsonify,
-    request
+    render_template
 )
 
 app = Flask(__name__)
@@ -11,19 +8,6 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("chat.html")
-
-@app.route("/getData", methods =["GET","POST"])
-def fetch():
-    try:
-        data = request.json
-        message = data.get("message")
-        url = f"http://api.brainshop.ai/get?bid=180356&key=6DRvcrqFlApaokis&uid=1&msg={message}"
-        res = requests.get(url)
-        response = res.json()
-        return jsonify(response)
-    except:
-        print("error")
-
 
 if __name__ == "__main__":
     app.run(port = 1234, debug = True)
